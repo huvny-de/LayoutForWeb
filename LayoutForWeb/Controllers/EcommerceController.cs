2d6374a87647280daac1c4dc786e97e62d3ab684
huvny <henri.deaaron@gmail.com>
2022-11-25 23:14:24 +0700

    add pages

---
+++ LayoutForWeb/Controllers/EcommerceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutForWeb.Controllers
+{
+    public class EcommerceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
